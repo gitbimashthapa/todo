81cb3ee712d89bc45c48c604060f3f906141af09
gitbimashthapa <mailbimashthapa@gmail.com>
2025-07-11 19:32:35 +0545

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Register from './pages/Register'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
